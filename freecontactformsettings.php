29e3da8a1971fd00f020b8cf57230ed0bfa12ec0
Tim Erickson <tim@triplo.co>
2018-04-04 12:35:33 -0500

    INitial commit

---
+++ freecontactformsettings.php
@@ -0,0 +1,11 @@
+<?php
+
+$email_to      = "help@yansplace.com";// your email address
+$email_subject = "Yans Place Contact Form";// email subject line
+$thankyou      = "thankyou.htm";// thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+?>
